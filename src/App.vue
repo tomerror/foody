b00fdcaa3952856af33bd867fbb9790c98109f79
Tomer Rozentzvaig <rozentzvaig@gmail.com>
2021-11-21 19:13:54 +0200

    adding svg

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,41 @@
 <template>
   <div id="app-grid">
     <div id="panel"></div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar  id="nav" />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from './views/navbar.vue';
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100vh;
 }
 #app-grid {
   display: grid;
   grid-template-columns: 7rem 1fr;
   grid-template-rows: 5rem 1fr;
+  height: inherit;
+  overflow: hidden;
 }
 #panel {
-    background: #262626;
+    background: #1b4332;
     grid-column: 1/3;
-}
-#nav {
-    display: grid;
-    background-image: linear-gradient(136deg, #579952, #4b9145);
-    color: white;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/views/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <router-link to="/"><img class="svg" src="fruits.svg" alt="fruits"></router-link>
+      <router-link to="/about"><img class="svg" src="vegetables.svg" alt="vegetables"></router-link>
+      <router-link to="/about"><img class="svg" src="bread.svg" alt="breads"></router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped lang="scss">
+#nav {
+    display: grid;
+    background-color: #2d6a4f;
+    color: white;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.svg {
+  height: 50px;
+
+  svg > g {
+    fill: white;
+  }
+}
+</style>
